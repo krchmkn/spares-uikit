74139bc4bc6a6db1a8f78369f9c69176aa9ec7b6
korchemkin <dmitri.korchemkin@gmail.com>
2017-03-09 23:08:10 +0300

    Update list

--- src/list/List.js
+++ src/list/List.js
@@ -8,42 +8,56 @@ import React, { Component } from 'react';
 import './List.css';
 
 class List extends Component {
-  render() {
-    let items = this.props.items || [];
-    let template = this.props.template || [];
-    let message = `Spares uikit: <List /> component template should be an array of objects.
-    Array item should have keys: 'label' and 'key'. Values of keys should be a string.
-    Value in 'key' should be the key in items={}.
-    Example: [
-      {
-        label: '',
-        key: ''
-      }, {
-        label: '',
-        key: ''
-      }
+
+  checkTemplate(template) {
+    let message = `Spares uikit: <List /> component template should be an array of items.
+    Item in array should be an array of objects. Object should have keys: 'label' and 'key'.
+    Values of keys should be a strings. Value in 'key' should be the key in items={}.
+    Example:
+    [
+      [ { label: 'My name is', key: 'first_name' }, { label: 'My surname is', key: 'second_name' } ],
+      [ { label: 'I am from', key: 'address' } ]
     ]\n`;
 
-    for (let t of template) {
-      if (typeof t !== 'object') {
+    for (let tmpl of template) {
+      if (typeof tmpl !== 'object') {
         throw new Error(message);
       } else {
-        if (typeof t.label === 'object') {
+        if (tmpl.length == null || tmpl.length === 0) {
           throw new Error(message);
-        } else if (typeof t.key === 'object') {
-          throw new Error(message);
+        } else {
+          for (let t of tmpl) {
+            if (typeof t.label === 'object') {
+              throw new Error(message);
+            } else if (typeof t.key === 'object') {
+              throw new Error(message);
+            } else {
+              return true;
+            }
+          }
         }
       }
     }
+  }
+
+  render() {
+    let items = this.props.items || [];
+    let template = this.props.template || [];
+
+    this.checkTemplate(template);
 
     return (
       <ul className="spares-list">
         {items.map((item, index) =>
           <li key={index} className="spares-list-item">
-            {template.map((val, i) =>
-              <div key={i} className="spares-list-unit">
-                <span className="spares-list-label">{val.label}: </span>
-                <span>{item[val.key]}</span>
+            {template.map((val, indx) =>
+              <div key={indx} className="spares-list-unit">
+                {val.map((v, i) =>
+                  <div key={i}>
+                    <span className="spares-list-label">{v.label}: </span>
+                    <span>{item[v.key]}</span>
+                  </div>
+                )}
               </div>
             )}
           </li>
