85ea1487fed29ed8b8f11d0bc1e3bb11a7c7750f
dmitri korchemkin <dmitri.korchemkin@gmail.com>
2017-03-16 18:09:33 +0300

    Update Nav styles

--- src/index.js
+++ src/index.js
@@ -7,8 +7,497 @@ import { Block, Button, Header, List,
         Radio, Checkbox, Dropdown,
         Card, Modal, Nav, Table } from '../spares';
 
+import './index.css';
+
+const listItems = [{
+    id: 1,
+    name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+  }, {
+    id: 2,
+    name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+}];
+
+const listWithImageItems = [{
+    name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    pageLink: '/pagelink',
+    image: 'https://cdn.worldvectorlogo.com/logos/react.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    label1: 'text1',
+    label2: 'text2'
+  }, {
+    name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    pageLink: '/pagelink',
+    image: 'https://cdn.worldvectorlogo.com/logos/react.svg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    label1: 'text3',
+    label2: 'text4'
+}];
+
+const tableItems = [{
+    'client_unique':null,
+    'client_clicks': 1000,
+    'client_visit':2562,
+    'order_count':73,
+    'wait_count':42,
+    'cancel_count':29,
+    'accepted_count':2,
+    'order_debug_count':0,
+    'accepted_seller_commission':1400.00,
+    'accepted_partner_profit':1200.00,
+    'accepted_system_profit':200.00,
+    'wait_seller_commission':33820.00,
+    'wait_partner_profit':29600.00,
+    'wait_system_profit':4220.00,
+    'cancel_seller_commission':23080.00,
+    'cancel_partner_profit':20200.00,
+    'cancel_system_profit':2880.00,
+    'max_date_group':1481625525,
+    'group_name':'13-12-2016',
+    'conversion':0.3724,
+    'partner_profit_from_unique_client':0.061218,
+    'approve':2.7397,
+    'wait':57.5342
+  }, {
+    'client_unique':null,
+    'client_clicks': 3000,
+    'client_visit':3562,
+    'order_count':83,
+    'wait_count':52,
+    'cancel_count':39,
+    'accepted_count':6,
+    'order_debug_count':3,
+    'accepted_seller_commission':2400.00,
+    'accepted_partner_profit':2200.00,
+    'accepted_system_profit':400.00,
+    'wait_seller_commission':53820.00,
+    'wait_partner_profit':79600.00,
+    'wait_system_profit':8220.00,
+    'cancel_seller_commission':12080.00,
+    'cancel_partner_profit':40200.00,
+    'cancel_system_profit':4880.00,
+    'max_date_group':625525,
+    'group_name':'13-12-2016',
+    'conversion':1.3724,
+    'partner_profit_from_unique_client':1.061218,
+    'approve':4.7397,
+    'wait':67.5342
+}, {
+  'client_unique':null,
+  'client_clicks': 2000,
+  'client_visit':3562,
+  'order_count':83,
+  'wait_count':52,
+  'cancel_count':39,
+  'accepted_count':6,
+  'order_debug_count':3,
+  'accepted_seller_commission':2400.00,
+  'accepted_partner_profit':2200.00,
+  'accepted_system_profit':400.00,
+  'wait_seller_commission':53820.00,
+  'wait_partner_profit':79600.00,
+  'wait_system_profit':8220.00,
+  'cancel_seller_commission':12080.00,
+  'cancel_partner_profit':40200.00,
+  'cancel_system_profit':4880.00,
+  'max_date_group':625525,
+  'group_name':'13-12-2016',
+  'conversion':1.3724,
+  'partner_profit_from_unique_client':1.061218,
+  'approve':4.7397,
+  'wait':67.5342
+}];
+
+
 ReactDOM.render(
   <div>
+
+    <a href="#pageHeader" className="spares-demo-to-menu"
+    title="to menu">
+      <i className="icon ion-arrow-up-a"></i>Menu
+    </a>
+
+    <div id="pageHeader" className="spares-demo-menu">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <Block>
+        <Nav items={[
+          {link: '#Blocks', text: 'Blocks'},
+          {link: '#Typography', text: 'Typography'},
+          {link: '#Card', text: 'Card'},
+          {link: '#Buttons', text: 'Buttons'},
+          {link: '#InputField', text: 'InputField'},
+          {link: '#SelectField', text: 'SelectField'},
+          {link: '#TextField', text: 'TextField'},
+          {link: '#Checkbox', text: 'Checkbox'},
+          {link: '#Radio', text: 'Radio'},
+          {link: '#Dropdown', text: 'Dropdown'},
+          {link: '#List', text: 'List'},
+          {link: '#ListWithImage', text: 'ListWithImage'},
+          {link: '#Modal', text: 'Modal'},
+          {link: '#Nav', text: 'Nav'},
+          {link: '#Table', text: 'Table'}
+        ]} />
+      </Block>
+    </div>
+
+
+    <div id="Blocks">
+      <Block>
+        <Header>Blocks</Header>
+      </Block>
+
+      <Block>
+        <Header small>Block</Header>
+      </Block>
+      <Block>Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua</Block>
+
+      <Block>
+        <Header small>Block flex</Header>
+      </Block>
+      <Block flex>
+        <div>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua</div>
+        <div>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua</div>
+        <div> Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua</div>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Typography">
+      <Block>
+          <Header>Typography</Header>
+
+          <Header>H1 header</Header>
+          <Header center>H1 header, text align center</Header>
+          <Header medium>H2 header</Header>
+          <Header small>H3 header</Header>
+          <Paragraph>
+            Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua
+          </Paragraph>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Card">
+      <Block>
+        <Header>Card</Header>
+      </Block>
+      <Block>
+        <Card>
+          Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua
+        </Card>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Buttons">
+      <Block>
+        <Header>Buttons</Header>
+      </Block>
+      <Block>
+        <Button onclick={() => alert('Spares-uikit')}>Click me</Button>
+        <Button submit>Submit button</Button>
+        <Button color="primary">Primary button</Button>
+        <Button color="danger">Danger button</Button>
+
+        <Header small>Button with icon</Header>
+        <Button icon="icon ion-loop" color="primary">Refresh</Button>
+
+        <Header small>Button disabled</Header>
+        <Button disabled>I'm disabled</Button>
+        <Button color="primary" disabled>I'm disabled</Button>
+        <Button color="danger" disabled>I'm disabled</Button>
+
+        <Header small>ButtonsTwins</Header>
+        <ButtonsTwins items={[
+          {
+            text: 'Button1',
+            onclick: () => alert('Spares-uikit: Button1')
+          }, {
+            text: 'Button2',
+            onclick: () => alert('Spares-uikit: Button2')
+          }
+        ]}/>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="InputField">
+      <Block>
+        <Header>InputField</Header>
+      </Block>
+      <Block>
+        <InputField
+              label="Input digits"
+              name="name"
+              placeholder="I'm placeholder"
+              pattern={/^\d+$/}
+              message="Only digits" />
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="SelectField">
+      <Block>
+        <Header>SelectField</Header>
+      </Block>
+      <Block>
+        <SelectField
+            label="Select"
+            name="select_name"
+            onchange={() => alert('Spares-uikit: SelectField')}
+            items={[
+              {value: 'value1', text: 'value 1'},
+              {value: 2, text: 2},
+              {value: 'value3', text: 'value 3'}
+            ]} />
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="TextField">
+      <Block>
+        <Header>TextField</Header>
+      </Block>
+      <Block>
+        <TextField
+            label="Enter text"
+            name="name"
+            placeholder="Enter 150 characters"
+            char_count={150}
+            message="Enter 150 characters" />
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Checkbox">
+      <Block>
+        <Header>Checkbox</Header>
+      </Block>
+      <Block>
+        <Checkbox items={listItems}
+              template={{
+                name: 'checkbox',
+                keysInItems: {
+                  value: 'name',
+                  text: 'address'
+                }
+              }} />
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Radio">
+      <Block>
+        <Header>Radio</Header>
+      </Block>
+      <Block>
+        <Radio items={listItems}
+              template={{
+                name: 'test',
+                keysInItems: {
+                  value: 'name',
+                  text: 'address'
+                }
+              }} />
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Dropdown">
+      <Block>
+        <Header>Dropdown</Header>
+      </Block>
+      <Block>
+        <Dropdown items={[
+          {text: 'item 1', onclick: () => alert('Spares-uikit: 1')},
+          {text: 'item 2', onclick: () => alert('Spares-uikit: 2')},
+          {text: 'item 3', onclick: () => alert('Spares-uikit: 3')}]}>
+            <Button icon="icon ion-android-arrow-dropdown">Click me</Button>
+        </Dropdown>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="List">
+      <Block>
+        <Header>List</Header>
+      </Block>
+      <Block>
+        <List items={listItems} template={[
+          [ {label: 'name', key: 'name'}, {label: 'name1', key: 'name'} ],
+          [ {label: 'description', key: 'description'} ],
+          [ {label: 'address', key: 'address'} ]
+        ]}/>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="ListWithImage">
+      <Block>
+        <Header>ListWithImage</Header>
+      </Block>
+      <Block>
+          <ListWithImage items={listWithImageItems} template={{
+            header: 'name',
+            pageLink: 'pagelink',
+            image: 'image',
+            description: 'description',
+            labels: [
+              {label: 'first line', key: 'label1'},
+              {label: 'second line', key: 'label2'},
+              {label: 'first line1', key: 'label1'},
+              {label: 'second line2', key: 'label2'}
+            ]
+          }}/>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Modal">
+      <Block>
+        <Header>Modal</Header>
+      </Block>
+      <Block>
+          <Modal header="Modal"
+                  buttonText="Оpen мodal"
+                  closeIcon="icon ion-close">
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+              address: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </div>
+          </Modal>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Nav">
+      <Block>
+        <Header>Nav</Header>
+      </Block>
+      <Block>
+        <Nav items={[
+          {text: 'page 1', submenu: [
+            {link: '/subpage', text: 'subpage'},
+            {link: '/subpage', text: 'subpage'},
+            {link: '/subpage', text: 'subpage'},
+            {link: '/subpage', text: 'subpage'}
+          ]},
+          {text: 'page 2', submenu: [
+            {link: '/subpage', text: 'subpage'},
+            {link: '/subpage', text: 'subpage'}
+          ]},
+          {link: '/page', text: 'page 3'},
+          {link: '/page', text: 'page 4'},
+          {link: '/page', text: 'page 5'},
+          {link: '/page', text: 'page 6'}
+        ]}
+        dropdownIcon="icon ion-android-arrow-dropdown"/>
+      </Block>
+    </div>
+
+    <br />
+
+    <div id="Table">
+      <Block>
+        <Header>Table</Header>
+      </Block>
+      <Block>
+          <Table head={[
+                  [
+                    [{text: ''}],
+                    [{text: 'Traffic'}],
+                    [{text: 'Indicators'}],
+                    [{text: 'Conversion'}],
+                    [{text: 'Finance'}]
+                  ],
+                  [
+                    [{text: 'Date'}],
+                    [
+                      {text: 'Clicks', sort: 'client_clicks'},
+                      {text: 'Unique clicks', sort: 'client_unique'},
+                      {text: 'Conversion', sort: 'conversion'}
+                    ],
+                    [
+                      {text: 'ECPC', sort: 'partner_profit_from_unique_client'},
+                      {text: 'Approve', sort: 'approve'},
+                      {text: 'Waiting', sort: 'wait_count'},
+                      {text: 'Orders', sort: 'order_count'}
+                    ],
+                    [
+                      {text: 'Waiting', sort: 'wait'},
+                      {text: 'Accepted', sort: 'accepted_count'},
+                      {text: 'Canceled', sort: 'cancel_count'},
+                      {text: 'Trash', sort: 'order_debug_count'}
+                    ],
+                    [
+                      {text: 'Enrolled', sort: 'accepted_count'},
+                      {text: 'Pending', sort: 'cancel_count'},
+                      {text: 'Rejected', sort: 'order_debug_count'}
+                    ]
+                  ]
+                ]}
+                body={{
+                  items: tableItems,
+                  template: [
+                    ['group_name'],
+                    ['client_clicks', 'client_unique', 'conversion'],
+                    ['partner_profit_from_unique_client', 'approve', 'wait_count', 'order_count'],
+                    ['wait', 'accepted_count', 'cancel_count', 'order_debug_count'],
+                    ['accepted_count', 'cancel_count', 'order_debug_count']
+                  ]
+                }}
+                total={[
+                  ['Total:'],
+                  ['client_clicks', 'client_unique', 'conversion'],
+                  ['partner_profit_from_unique_client', 'approve', 'wait_count', 'order_count'],
+                  ['wait', 'accepted_count', 'cancel_count', 'order_debug_count'],
+                  ['accepted_count', 'cancel_count', 'order_debug_count']
+                ]} />
+      </Block>
+    </div>
+
+    <br />
 
   </div>,
   document.getElementById('root')
